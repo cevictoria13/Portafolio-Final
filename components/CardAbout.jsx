64e0c5af56201bcb090dd7826cd8e033ea962b78
Cecilia Sánchez <cevictoria@gmail.com>
2024-02-23 00:15:29 -0300

    finalizando portafolio v.1.0

--- components/CardAbout.jsx
+++ components/CardAbout.jsx
@@ -8,26 +8,33 @@ import { fadeIn } from "@/components/utils/motionTransitions";
 const CardAbout = () => {
     return (
         <motion.div
-        variants={fadeIn("down", 0.5)}
-        initial="hidden"
-        animate="show"
-        exit="hidden" 
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
 
-        className='h-screen flex flex-col justify-center items-center'>
-            <Image           
+            className='h-screen flex flex-col justify-center items-center'>
+            <Image
                 isBlurred
                 className='mb-5'
                 width={300}
                 src={profile.src}
                 alt="foto de perfil" />
             <div>
-                <h1 className='text-4xl mb-5
-                 font-bold text-secondary'>About</h1>
+                <h1 className='text-4xl mb-5 mt-5
+                 font-bold text-secondary'>About me&us</h1>
             </div>
 
             <div>
                 <p className='text-xl px-5 sm:px-20'>
-                    Más de 15 años de experiencia en diseño de productos y servicios como también en gestión de proyectos ambientales, sociales, de fomento, entre otros. Actualmente, incorporamos el servicio de desarrollo web full stack para entregar soluciones funcionales y atractivas a nuestros clientes. Contáctanos para entregarte una solución a tu medida.
+                    Más de 15 años de experiencia en diseño de productos y servicios como también en gestión de proyectos. Actualmente, incorporamos el servicio de desarrollo frontend y backend, enfocado en la construcción de aplicaciones web para entregar soluciones funcionales y atractivas a nuestros clientes, con herramientas como: HTML, JS, Css, React, bootstrap, next y tailwind.
+
+                </p>
+            </div>
+
+            <div>
+                <p className='text-2xl mb-5 mt-5 font-bold text-secondary px-5 sm:px-20'>
+                    Contáctanos para desarrollar una solución a tu medida.
                 </p>
             </div>
 
